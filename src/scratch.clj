19871e382897e9a5eb75a6f4bdeb0e90bc2bb455
Keith Harper <keithharper1155@gmail.com>
2021-10-04 12:00:03 -0400

    Reformat

--- src/scratch.clj
+++ src/scratch.clj
@@ -1,51 +1,51 @@
 (ns scratch)
 
 (defn pull-keys
-      "Returns a map containing only those entries in map whose key is in pull-selector,
-      recursively applying to nested maps.
+  "Returns a map containing only those entries in map whose key is in pull-selector,
+  recursively applying to nested maps.
 
-      `pull-selector` takes the same shape as Datomic's pull selector and similar opts:
-      `:xform` - a function that will be applied to the corresponding value
-      `:as` - replaces the respective key with the provided value
-      `:default` - default value to use if a value isn't found for the respective key"
-      [m pull-selector]
-      (let [pull-opt? #{:as :xform :default}
-            opts?     (every-pred (some-fn vector? list?)
-                                  #(filter pull-opt? %))
-            k?        (some-fn (complement coll?)
-                               opts?)]
-           (reduce (fn [acc k-or-kopts]
-                       (let [k-fn (fn [k-or-kopts*]
-                                      (if (opts? k-or-kopts*)
-                                        (let [[k & {:keys [as]}] k-or-kopts*]
-                                             (or as k))
-                                        k-or-kopts*))
-                             v-fn (fn [m* k-or-kopts*]
-                                      (if (opts? k-or-kopts*)
-                                        (let [[k & {:keys [xform default]}] k-or-kopts*]
-                                             (cond-> m*
-                                                     (some? k)
-                                                     (get k default)
+  `pull-selector` takes the same shape as Datomic's pull selector and similar opts:
+  `:xform` - a function that will be applied to the corresponding value
+  `:as` - replaces the respective key with the provided value
+  `:default` - default value to use if a value isn't found for the respective key"
+  [m pull-selector]
+  (let [pull-opt? #{:as :xform :default}
+        opts?     (every-pred (some-fn vector? list?)
+                              #(filter pull-opt? %))
+        k?        (some-fn (complement coll?)
+                           opts?)]
+    (reduce (fn [acc k-or-kopts]
+              (let [k-fn (fn [k-or-kopts*]
+                           (if (opts? k-or-kopts*)
+                             (let [[k & {:keys [as]}] k-or-kopts*]
+                               (or as k))
+                             k-or-kopts*))
+                    v-fn (fn [m* k-or-kopts*]
+                           (if (opts? k-or-kopts*)
+                             (let [[k & {:keys [xform default]}] k-or-kopts*]
+                               (cond-> m*
+                                 (some? k)
+                                 (get k default)
 
-                                                     (some? xform)
-                                                     (xform)))
-                                        (get m* k-or-kopts*)))]
-                            (cond
-                              (k? k-or-kopts)
-                              (if-some [v (v-fn m k-or-kopts)]
-                                       (assoc acc (k-fn k-or-kopts) v)
-                                       acc)
+                                 (some? xform)
+                                 (xform)))
+                             (get m* k-or-kopts*)))]
+                (cond
+                  (k? k-or-kopts)
+                  (if-some [v (v-fn m k-or-kopts)]
+                    (assoc acc (k-fn k-or-kopts) v)
+                    acc)
 
-                              (map? k-or-kopts)
-                              (reduce-kv (fn [acc* k-or-kopts* selector]
-                                             (let [k (k-fn k-or-kopts*)]
-                                                  (if-some [v* (pull-keys (v-fn m k-or-kopts*) selector)]
-                                                           (assoc acc* k v*)
-                                                           acc*)))
-                                         acc
-                                         k-or-kopts))))
-                   {}
-                   pull-selector)))
+                  (map? k-or-kopts)
+                  (reduce-kv (fn [acc* k-or-kopts* selector]
+                               (let [k (k-fn k-or-kopts*)]
+                                 (if-some [v* (pull-keys (v-fn m k-or-kopts*) selector)]
+                                   (assoc acc* k v*)
+                                   acc*)))
+                             acc
+                             k-or-kopts))))
+            {}
+            pull-selector)))
 
 (comment
   ;; xform to-datomic
@@ -56,7 +56,7 @@
   (pull-keys {:test/level      0
               :test/other-next {:test/level 10
                                 :test/names ["name1" "name2"]}
-              :test/_students       [{:teacher/id 2 :teacher/name "level 2"}]
+              :test/_students  [{:teacher/id 2 :teacher/name "level 2"}]
               :some.other/foo  "bar"
               :test/next       {:test/level 1
                                 :test/name  "level 1"
